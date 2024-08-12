3241a0d0e90c6df0b3509306664de327125547ca
lee juyoung <juyung0903@naver.com>
2024-08-12 17:19:53 +0900

    feat: 그룹 등록 페이지 폼 구성 및 그룹명, 그룹인원 입력창

---
+++ src/app/(afterLogin)/group/register/_component/GroupForm.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
+
+const formSchema = z.object({
+  groupName: z
+    .string()
+    .min(2, {
+      message: 'groupName must be at least 2 characters.',
+    })
+    .max(10),
+  groupCapacity: z
+    .number()
+    .min(1, { message: 'groupCapacity must be at least 1 characters' })
+    .max(12),
+  address: z.string(),
+  description: z
+    .string()
+    .min(5, { message: '최소 5글자 이상은 적어주세요.' })
+    .max(100),
+});
+
+export function GroupForm() {
+  // 1. Define your form.
+  const MAX_CAPACITY = 10;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      groupName: '',
+      groupCapacity: 1,
+    },
+  });
+  const groupCapacitys: number[] = [];
+  for (let i = 1; i <= MAX_CAPACITY; i++) {
+    groupCapacitys.push(i);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="groupName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>그룹명</FormLabel>
+              <FormControl>
+                <Input placeholder="그룹명을 입력해주세요" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="groupCapacity"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>그룹 인원</FormLabel>
+              <Select onValueChange={field.onChange}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="인원 수를 선택해주세요" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {groupCapacitys.map((number) => (
+                    <SelectItem key={number} value={`${number}`}>
+                      {number}명
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+        
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+}
--- src/app/(afterLogin)/group/register/page.tsx
+++ src/app/(afterLogin)/group/register/page.tsx
@@ -1,7 +1,9 @@
+import { GroupForm } from "./_component/GroupForm";
+
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between">
-      그룹등록 페이지
+      <GroupForm />
     </main>
   );
 }
