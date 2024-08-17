3702afd9164ee54b78e217eb2be41513853de547
lee juyoung <juyung0903@naver.com>
2024-08-17 17:21:55 +0900

    refactor: 그룹 등록 페이지 formSchema분리

--- src/app/(afterLogin)/group/register/_component/GroupForm.tsx
+++ src/app/(afterLogin)/group/register/_component/GroupForm.tsx
@@ -25,56 +25,56 @@ import {
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { Checkbox } from '@/components/ui/checkbox';
-// import { formSchema } from '../_lib/GroupFormSchema';
+import { formSchema } from '../_lib/GroupFormSchema';
 import { DateTimePicker } from './time-picker/DateTimePicker';
 import { groupCapacitys } from '../_lib/Constants';
 
-const formSchema = z
-  .object({
-    name: z
-      .string()
-      .min(2, {
-        message: '그룹 이름은 최소 1글자 이상이어야 합니다.',
-      })
-      .max(10),
-    groupCapacity: z.string().superRefine((data, ctx) => {
-      if (1 > Number(data) || Number(data) > 12) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: '그룹 인원수는 1명에서 12명까지만 가능합니다.',
-        });
-      }
-    }),
-    address: z.string().min(10, {
-      message: '그룹 이름은 최소 10글자 이상이어야 합니다.',
-    }),
-    description: z
-      .string()
-      .min(5, { message: '그룹 설명은 최소 5글자 이상은 적어주세요.' })
-      .max(100, { message: '그룹 설명은 최소 100글자를 넘을 수는 없습니다.' }),
-    beginAt: z
-      .date()
-      .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
-    endAt: z
-      .date()
-      .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
-    isAgree: z.boolean()
-  }).superRefine((data, ctx) => {
-    if (!data.isAgree) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        path: ['isAgree'],
-        message: '약관에 동의하셔야 그룹이 생성됩니다.',
-      });
-    }
-    if (data.endAt <= data.beginAt) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        path: ['endAt'],
-        message: '종료일자는 시작일자보다 이후여야 합니다.',
-      });
-    }
-  });
+// const formSchema = z
+//   .object({
+//     name: z
+//       .string()
+//       .min(2, {
+//         message: '그룹 이름은 최소 1글자 이상이어야 합니다.',
+//       })
+//       .max(10),
+//     groupCapacity: z.string().superRefine((data, ctx) => {
+//       if (1 > Number(data) || Number(data) > 12) {
+//         ctx.addIssue({
+//           code: z.ZodIssueCode.custom,
+//           message: '그룹 인원수는 1명에서 12명까지만 가능합니다.',
+//         });
+//       }
+//     }),
+//     address: z.string().min(10, {
+//       message: '그룹 이름은 최소 10글자 이상이어야 합니다.',
+//     }),
+//     description: z
+//       .string()
+//       .min(5, { message: '그룹 설명은 최소 5글자 이상은 적어주세요.' })
+//       .max(100, { message: '그룹 설명은 최소 100글자를 넘을 수는 없습니다.' }),
+//     beginAt: z
+//       .date()
+//       .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
+//     endAt: z
+//       .date()
+//       .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
+//     isAgree: z.boolean()
+//   }).superRefine((data, ctx) => {
+//     if (!data.isAgree) {
+//       ctx.addIssue({
+//         code: z.ZodIssueCode.custom,
+//         path: ['isAgree'],
+//         message: '약관에 동의하셔야 그룹이 생성됩니다.',
+//       });
+//     }
+//     if (data.endAt <= data.beginAt) {
+//       ctx.addIssue({
+//         code: z.ZodIssueCode.custom,
+//         path: ['endAt'],
+//         message: '종료일자는 시작일자보다 이후여야 합니다.',
+//       });
+//     }
+//   });
 
 export function GroupForm() {
   const form = useForm<z.infer<typeof formSchema>>({
--- src/app/(afterLogin)/group/register/_lib/GroupFormSchema.ts
+++ src/app/(afterLogin)/group/register/_lib/GroupFormSchema.ts
@@ -1,51 +1,48 @@
 import { z } from "zod";
 
 export const formSchema = z
-  .object({
-    name: z
-      .string()
-      .min(2, {
-        message: '그룹 이름은 최소 1글자 이상이어야 합니다.',
-      })
-      .max(10),
-    groupCapacity: z.string().superRefine((data, ctx) => {
-      if (1 > Number(data) || Number(data) > 12) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: '그룹 인원수는 1명에서 12명까지만 가능합니다.',
-        });
-      }
-    }),
-    address: z.string().min(10, {
-      message: '그룹 이름은 최소 10글자 이상이어야 합니다.',
-    }),
-    latitude: z.number(),
-    longitude: z.number(),
-    description: z
-      .string()
-      .min(5, { message: '그룹 설명은 최소 5글자 이상은 적어주세요.' })
-      .max(100, { message: '그룹 설명은 최소 100글자를 넘을 수는 없습니다.' }),
-    beginAt: z
-      .date()
-      .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
-    endAt: z
-      .date()
-      .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
-    isAgree: z.boolean(),
-  })
-  .superRefine((data, ctx) => {
-    if (!data.isAgree) {
+.object({
+  name: z
+    .string()
+    .min(2, {
+      message: '그룹 이름은 최소 1글자 이상이어야 합니다.',
+    })
+    .max(10),
+  groupCapacity: z.string().superRefine((data, ctx) => {
+    if (1 > Number(data) || Number(data) > 12) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
-        path: ['isAgree'],
-        message: '약관에 동의하셔야 그룹이 생성됩니다.',
+        message: '그룹 인원수는 1명에서 12명까지만 가능합니다.',
       });
     }
-    if (data.endAt <= data.beginAt) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        path: ['endAt'],
-        message: '종료일자는 시작일자보다 이후여야 합니다.',
-      });
-    }
-  });
+  }),
+  address: z.string().min(10, {
+    message: '그룹 이름은 최소 10글자 이상이어야 합니다.',
+  }),
+  description: z
+    .string()
+    .min(5, { message: '그룹 설명은 최소 5글자 이상은 적어주세요.' })
+    .max(100, { message: '그룹 설명은 최소 100글자를 넘을 수는 없습니다.' }),
+  beginAt: z
+    .date()
+    .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
+  endAt: z
+    .date()
+    .min(new Date(), { message: '현재 시간보다 과거에 만날 수는 없습니다.' }),
+  isAgree: z.boolean()
+}).superRefine((data, ctx) => {
+  if (!data.isAgree) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      path: ['isAgree'],
+      message: '약관에 동의하셔야 그룹이 생성됩니다.',
+    });
+  }
+  if (data.endAt <= data.beginAt) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      path: ['endAt'],
+      message: '종료일자는 시작일자보다 이후여야 합니다.',
+    });
+  }
+});
