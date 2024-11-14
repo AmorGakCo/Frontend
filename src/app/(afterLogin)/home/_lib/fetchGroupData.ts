9cd1b45a9743cde1e50bceea03180a54745fec42
lee juyoung <juyung0903@naver.com>
2024-11-14 17:47:41 +0900

    fix: api 주소 수정 및 토큰추가 변경사항 반영

--- src/app/(afterLogin)/home/_lib/fetchGroupData.ts
+++ src/app/(afterLogin)/home/_lib/fetchGroupData.ts
@@ -1,8 +1,12 @@
 export async function fetchGroupData(groupId: number) {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_LOCATION}/groups/basic/${groupId}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_LOCATION}/groups/${groupId}/basic`, {
       method: "GET",
       cache: "no-cache",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      },
     })
     if (!response.ok) {
       throw new Error(`Error: ${response.statusText}`);
