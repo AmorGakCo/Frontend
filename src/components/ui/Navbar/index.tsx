f36d7086b2fea1f5b3c68a14f404b057a06d891e
lee juyoung <juyung0903@naver.com>
2024-11-24 00:25:10 +0900

    fix: Header,Navbar에 hydrate 에러 나오지 않도록 수정

--- src/components/ui/Navbar/index.tsx
+++ src/components/ui/Navbar/index.tsx
@@ -1,19 +1,31 @@
 'use client';
 
-import Button from "./Button";
-import GroupRegisterButton from "./Button/GroupRegisterButton";
+import { useEffect, useState } from 'react';
+import Button from './Button';
+import GroupRegisterButton from './Button/GroupRegisterButton';
+import Cookies from 'js-cookie';
 export default function NavBar() {
+  const [accessToken,setAccessToken] = useState(Cookies.get('accessToken'));
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // 클라이언트에서만 동작하도록 설정
+  }, []);
   return (
-    <div className="w-full h-navbarHeight shadow-navbar px-[15px] pb-[6.5px] pt-[2.5px] flex lg:gap-9 gap-1 justify-center items-center fixed bottom-0 z-20 bg-[#F2F2F2]">
-      <div className = 'flex lg:gap-10 gap-2'>
-      <Button link = '/home' name = '홈'/>
-      <Button link = '/ranking' name = '랭킹'/>
-      </div>
-      <GroupRegisterButton />
-      <div className = 'flex lg:gap-10 gap-2'>
-      <Button link = '/message' name = '채팅'/>
-      <Button link = '/group/history' name = '그룹'/>
-      </div>
-    </div>
+    <>
+    {isClient && accessToken && (
+        <div className="w-full h-navbarHeight shadow-navbar px-[15px] pb-[6.5px] pt-[2.5px] flex lg:gap-9 gap-1 justify-center items-center fixed bottom-0 z-20 bg-[#F2F2F2]">
+          <div className="flex lg:gap-10 gap-2">
+            <Button link="/home" name="홈" />
+            <Button link="/ranking" name="랭킹" />
+          </div>
+          <GroupRegisterButton />
+          <div className="flex lg:gap-10 gap-2">
+            <Button link="/message" name="채팅" />
+            <Button link="/group/history" name="그룹" />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
