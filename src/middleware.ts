b094e3fbc818861bd345652c0e6edeaf32ac2b57
lee juyoung <juyung0903@naver.com>
2024-11-24 00:33:47 +0900

    fix: middleware에서는 토큰 리프레쉬 로직만 하도록 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,24 @@
 import { getAccessTokenWithRefreshToken } from '@/lib/getAccessTokenWithRefreshToken';
 import { isTokenExpired } from '@/lib/isTokenExpired';
 import { NextRequest, NextResponse } from 'next/server';
-import Cookies from 'js-cookie';
 
 export default async function middleware(request: NextRequest) {
-  // 프론트 서버 요청을 완료하기 전 쿠키에서 accessToken을 가져와서 만료되었는지 확인
-  let token = request.cookies.get('accessToken');
-  const pathname = request.nextUrl.pathname;
-  if (!token) {
-    if (pathname === '/home') {
-      return NextResponse.redirect(new URL('/', request.url))
-    }
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
+  // 쿠키에서 accessToken 가져오기
+  let token = request.cookies.get('accessToken');  
+  // 토큰이 만료되었으면 새로운 accessToken을 발급받고 쿠키에 저장
   if (token && isTokenExpired(token.value)) {
     const result = await getAccessTokenWithRefreshToken();
-    if (result.data.status !== 400 && result.path === '/error') {
-      const response = NextResponse.next();
-      response.cookies.set('accessToken', result.accessToken);
-      return response;
-    } else {
-      return NextResponse.redirect(new URL('/login', request.url));
+    if (result.data.status === 'failure') {
+      return NextResponse.redirect(new URL('/login',request.url));
     }
-  }
+    const response = NextResponse.next();
+    response.cookies.set('accessToken',result.data.accessToken);
+    return response;
+  } 
+  // 토큰이 유효하면 요청을 계속 진행
+  return NextResponse.next();
 }
+
 
 // middleware가 적용될 페이지 경로를 정의
 export const config = {
