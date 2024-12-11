3fa96ba5ffaa1ab5fdc86a0923ba237bb28fa8fa
lee juyoung <juyung0903@naver.com>
2024-12-11 18:01:56 +0900

    feat: 서버 단에서 데이터요청할수있는 fetchAuthServer

---
+++ src/app/(afterLogin)/_lib/FetchWithAuthServer.ts
@@ -0,0 +1,32 @@
+import Cookies from "js-cookie";
+import { cookies } from "next/headers";
+interface ApiFetchOptions extends RequestInit {
+  headers?: Record<string, string>;
+}
+
+/**
+ * AccessToken이 포함된 fetch 요청 함수
+ * @param endpoint - API 엔드포인트
+ * @param options - fetch 옵션
+ * @returns API 응답의 JSON 데이터
+ */
+export const fetchWithAuthServer = async <T = any>(endpoint: string, options: ApiFetchOptions = {}): Promise<T> => {
+  const accessToken = cookies().toString().split('=')[1];
+  const headers: Record<string, string> = {
+    'Content-Type': 'application/json',
+    ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
+    ...options.headers,
+  };
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_LOCATION}/api${endpoint}`, {
+    ...options,
+    credentials: 'include',
+    headers,
+  });
+
+  if (!response.ok) {
+    throw new Error(`API 요청 실패: ${response.statusText}`);
+  }
+  const result = await response.json();
+  return result;
+};
