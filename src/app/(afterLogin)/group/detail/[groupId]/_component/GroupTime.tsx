e5e1bd4bed4737f41c93b1c6b868abc7fe4ae81a
lee juyoung <juyung0903@naver.com>
2024-12-11 18:40:49 +0900

    fix: 상세페이지 모임시간에서 시,분이 한자리수 일시 앞에 0이붙도록 수정

--- src/app/(afterLogin)/group/detail/[groupId]/_component/GroupTime.tsx
+++ src/app/(afterLogin)/group/detail/[groupId]/_component/GroupTime.tsx
@@ -20,8 +20,8 @@ export default function GroupTime ({groupId}:{groupId: number}) {
     <div className="flex justify-between items-center w-full h-10">
           <div>모임 시간</div>
           <div className="font-thin text-xl">
-            {`${beginAtDate.getHours()}:${beginAtDate.getMinutes()}`}~
-            {`${endAtDate.getHours()}:${endAtDate.getMinutes()}`}
+            {`${beginAtDate.getHours().toString().padStart(2,'0')}:${beginAtDate.getMinutes().toString().padStart(2,'0')}`}~
+            {`${endAtDate.getHours().toString().padStart(2,'0')}:${endAtDate.getMinutes().toString().padStart(2,'0')}`}
           </div>
         </div>
   )
