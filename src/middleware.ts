777de34c19b74e2ccad4f3e6b5f365ebc7c23976
lee juyoung <juyung0903@naver.com>
2024-12-11 17:20:24 +0900

    fix: 엑세스 토큰 갱신하는 함수가 엑세스토큰을 포함하지않고 요청보내도록 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,17 @@ export default async function middleware(request: NextRequest) {
   // 쿠키에서 accessToken 가져오기
   let token = request.cookies.get('accessToken');  
   // 토큰이 만료되었으면 새로운 accessToken을 발급받고 쿠키에 저장
+  
   if (token && isTokenExpired(token.value)) {
     const result = await getAccessTokenWithRefreshToken();
-    if (result.data.status === 'failure') {
+    console.log(result);
+    if (result.status !== 'success') {
       return NextResponse.redirect(new URL('/login',request.url));
     }
     const response = NextResponse.next();
     response.cookies.set('accessToken',result.data.accessToken);
     return response;
-  } 
-  // 토큰이 유효하면 요청을 계속 진행
-  return NextResponse.next();
+  }
 }
 
 
