941f4c4be577432be0b8d77151abbd5316ad14f6
lee juyoung <juyung0903@naver.com>
2024-12-16 13:27:55 +0900

    feat: accessToken자체가 유효하지 않을시 로그인페이지로

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,25 +2,30 @@ import { getAccessTokenWithRefreshToken } from '@/lib/getAccessTokenWithRefreshT
 import { isTokenExpired } from '@/lib/isTokenExpired';
 import { NextRequest, NextResponse } from 'next/server';
 
+function gotoLogin(request: NextRequest) {
+  const response = NextResponse.redirect(new URL('/login', request.url));
+  response.cookies.delete('accessToken');
+  response.cookies.delete('refresh-token');
+  return response;
+}
 export default async function middleware(request: NextRequest) {
   // 쿠키에서 accessToken 가져오기
-  let token = request.cookies.get('accessToken');  
+  let token = request.cookies.get('accessToken');
   // 토큰이 만료되었으면 새로운 accessToken을 발급받고 쿠키에 저장
-  
+  // 토큰이 유효한지
+  if (token && token.value.split('.').length !== 3) {
+    return gotoLogin(request);
+  }
   if (token && isTokenExpired(token.value)) {
-    const result = await getAccessTokenWithRefreshToken();
-    if (result.path === '/error') {
-      const response = NextResponse.redirect(new URL('/login',request.url));
-      response.cookies.delete('accessToken');
-      response.cookies.delete('refresh-token');
-      return response;
+    const result = await getAccessTokenWithRefreshToken(request);
+    if (result.status === '/failure') {
+      return gotoLogin(request);
     }
     const response = NextResponse.next();
-    response.cookies.set('accessToken',result.data.accessToken);
+    response.cookies.set('accessToken', result.data.accessToken);
     return response;
   }
 }
-
 
 // middleware가 적용될 페이지 경로를 정의
 export const config = {
