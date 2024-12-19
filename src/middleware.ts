92ad1574741ef804aaa0efe5c354411369dfb2f6
lee juyoung <juyung0903@naver.com>
2024-12-19 18:11:54 +0900

    fix: 로그인페이지로 리다이렉트 될 때 accessToken도 지우기

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,11 +14,13 @@ export default async function middleware(request: NextRequest) {
   // 토큰이 만료되었으면 새로운 accessToken을 발급받고 쿠키에 저장
   // 토큰이 유효한지
   if (token && token.value.split('.').length !== 3) {
+    request.cookies.delete('accessToken');
     return gotoLogin(request);
   }
   if (token && isTokenExpired(token.value)) {
     const result = await getAccessTokenWithRefreshToken(request);
     if (result.status === '/failure') {
+      request.cookies.delete('accessToken');
       return gotoLogin(request);
     }
     const response = NextResponse.next();
