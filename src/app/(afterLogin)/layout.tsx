24b3b61c6d846e3f53eefe4d13e3866ab853c977
lee juyoung <juyung0903@naver.com>
2024-12-27 18:01:20 +0900

    feat: 알림 데이터 프리페칭

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,22 +1,36 @@
-import { NotificationComponent } from "@/components/NotificationComponent";
-import NavBar from "@/components/ui/Navbar";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { NotificationComponent } from '@/components/NotificationComponent';
+import NavBar from '@/components/ui/Navbar';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { fetchNotificationServer } from './notification/_lib/fetchNotificationServer';
 export default async function RootLayout({
   children,
-
 }: Readonly<{
-  children: React.ReactNode,
+  children: React.ReactNode;
 }>) {
   const token = cookies().get('accessToken');
-  if(!token) {
+  if (!token) {
     redirect('/login');
   }
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['notification', { page: 0 }],
+    queryFn: fetchNotificationServer,
+  });
+  const data = queryClient.getQueryData(['notification', { page: 0 }]);
+  const dehydratedState = data ? dehydrate(queryClient) : null;
   return (
     <>
-    {children}
-    <NavBar/>
-    <NotificationComponent />
+      <HydrationBoundary state={dehydratedState}>
+        {children}
+        <NavBar />
+        <NotificationComponent />
+      </HydrationBoundary>
     </>
   );
 }
