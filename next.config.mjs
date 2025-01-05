ae84f1d88867cdac86334c854975cac6f88e3e69
lee juyoung <juyung0903@naver.com>
2025-01-05 22:39:17 +0900

    setting: next.config.mjs pwa 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: false, // Strict Mode 비활성화
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public', // PWA 리소스 저장 경로
+  register: true, // 서비스 워커 자동 등록
+  skipWaiting: true, // 새 서비스 워커 즉시 활성화
+})(nextConfig);
