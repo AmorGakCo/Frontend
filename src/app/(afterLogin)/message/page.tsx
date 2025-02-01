9b40b6b83338cfd12c9de45759f995949396ba1f
lee juyoung <juyung0903@naver.com>
2025-02-01 19:26:17 +0900

    chore: 채팅페이지 메시지 변경

--- src/app/(afterLogin)/message/page.tsx
+++ src/app/(afterLogin)/message/page.tsx
@@ -7,7 +7,7 @@ export async function generateMetadata () {
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-        채팅목록 페이지
+        준비중입니다!
     </main>
   );
 }
